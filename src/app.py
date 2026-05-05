ce42a4d6f37034d73d2928cf51bbb9bc97e67710
Sara-Barbosa-Silva <sara.barbosa.silvas@gmail.com>
2026-05-05 01:12:28 -0300

    feat: integrar painel do gerente e estilo ao app

--- src/app.py
+++ src/app.py
@@ -3,6 +3,11 @@ import streamlit as st
 from views.tela_login import exibir_tela_login
 from views.tela_cadastro import exibir_tela_cadastro
 from views.tela_cliente import exibir_tela_cliente
+from views.tela_gerente import exibir_tela_gerente
+from views.tela_estilo import aplicar_estilo
+
+from services.cardapio_service import carregar_cardapio, salvar_cardapio
+from services.pedido_service import carregar_pedidos, salvar_pedidos
 
 
 st.set_page_config(
@@ -10,11 +15,11 @@ st.set_page_config(
     layout="wide"
 )
 
+aplicar_estilo()
 
 st.title("FIAP Lanches")
 
 
-# Inicialização do session_state
 if "logado" not in st.session_state:
     st.session_state["logado"] = False
 
@@ -25,7 +30,6 @@ if "carrinho" not in st.session_state:
     st.session_state["carrinho"] = {}
 
 
-# Usuário não logado
 if not st.session_state["logado"]:
     menu = st.sidebar.radio("Menu", ["Login", "Cadastro"])
 
@@ -35,8 +39,6 @@ if not st.session_state["logado"]:
     elif menu == "Cadastro":
         exibir_tela_cadastro()
 
-
-# Usuário logado
 else:
     usuario_logado = st.session_state["usuario"]
 
@@ -49,8 +51,17 @@ else:
         st.session_state["carrinho"] = {}
         st.rerun()
 
-    if usuario_logado["perfil"] == "cliente":
-        exibir_tela_cliente()
+    if usuario_logado["perfil"] == "gerente":
+        exibir_tela_gerente(
+            carregar_pedidos,
+            salvar_pedidos,
+            carregar_cardapio,
+            salvar_cardapio
+        )
 
-    elif usuario_logado["perfil"] == "gerente":
-        st.info("Painel do gerente será implementado na próxima etapa.")
+    else:
+        exibir_tela_cliente(
+            carregar_cardapio,
+            carregar_pedidos,
+            salvar_pedidos
+        )
