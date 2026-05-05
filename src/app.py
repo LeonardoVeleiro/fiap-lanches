39b1a45297efcefd1df4a68d8e5bdbd4dcb3d69b
LeonardoVeleiro <163356452+LeonardoVeleiro@users.noreply.github.com>
2026-05-05 00:14:24 -0300

    feat: integrar fluxo do cliente ao app

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from services.banco_service import carregar_json, salvar_json
 from views.tela_login import exibir_tela_login
 from views.tela_cadastro import exibir_tela_cadastro
-
+from views.tela_cliente import exibir_tela_cliente
 
 ARQUIVO_CARDAPIO = "src/data/cardapio.json"
 ARQUIVO_PEDIDOS = "src/data/pedidos.json"
@@ -35,8 +35,13 @@ st.title("FIAP Lanches")
 
 if "logado" not in st.session_state:
     st.session_state["logado"] = False
+
 if "usuario" not in st.session_state:
     st.session_state["usuario"] = None
+
+if "carrinho" not in st.session_state:
+   st.session_state["carrinho"] = {}
+
 
 if not st.session_state["logado"]:
     menu = st.sidebar.radio("Menu", ["Login", "Cadastro"])
@@ -45,6 +50,23 @@ if not st.session_state["logado"]:
     elif menu == "Cadastro":
         exibir_tela_cadastro()
 else:
-    usuario_logado = st.session_state["usuario"]
-    st.sidebar.success(f"Logado como: {usuario_logado['nome']}")
-    st.write("Login realizado com sucesso. As telas de cliente e gerente serão adicionadas nas próximas etapas.")
+   usuario_logado = st.session_state["usuario"]
+
+   st.sidebar.success(f"Logado como: {usuario_logado['nome']}")
+   st.sidebar.write(f"Perfil: {usuario_logado['perfil']}")
+
+    if st.sidebar.button("Sair"):
+       st.session_state["logado"] = False
+       st.session_state["usuario"] = None
+       st.session_state["carrinho"] = {}
+       st.rerun()
+
+   if usuario_logado["perfil"] == "cliente":
+       exibir_tela_cliente(
+           carregar_cardapio,
+           carregar_pedidos,
+           salvar_pedidos
+       )
+   else:
+       st.info("Painel do gerente será implementado na próxima etapa.")
+
